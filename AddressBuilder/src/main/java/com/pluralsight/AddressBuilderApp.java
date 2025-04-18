0535ffe6ac568cec589870c870b490fd7234d2fb
Daniel Okonkwo <danielokonkwo.goz@gmail.com>
2025-04-18 10:28:45 -0500

    Completed exercise 5 - Address Builder

---
+++ AddressBuilder/src/main/java/com/pluralsight/AddressBuilderApp.java
@@ -0,0 +1,63 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class AddressBuilderApp {
+    static Scanner myScanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Please provide the following information:");
+        System.out.println("Full Name: (First and Last name Please.");
+        String fullName = myScanner.nextLine();
+        StringBuilder addressInfo = new StringBuilder();
+        StringBuilder shippingInfo = new StringBuilder();
+
+        System.out.println("Billing Street: ");
+        String billingStreet = myScanner.nextLine();
+
+
+        System.out.println("Billing City: ");
+        String billingCity = myScanner.nextLine();
+
+        System.out.println("Billing State: ");
+        String billingState = myScanner.nextLine();
+
+        System.out.println("Billing Zip: ");
+        String billingZip = myScanner.nextLine();
+
+        System.out.println("----------------------------------------");
+
+        System.out.println("Shipping Street: ");
+        String shippingStreet = myScanner.nextLine();
+
+
+        System.out.println("Shipping City: ");
+        String shippingCity = myScanner.nextLine();
+
+        System.out.println("Shipping State: ");
+        String shippingState = myScanner.nextLine();
+
+        System.out.println("Shipping Zip: ");
+        String shippingZip = myScanner.nextLine();
+
+        System.out.println(fullName);
+
+        addressInfo.append("Billing Address:\n ");
+        addressInfo.append(billingStreet).append("\n");
+        addressInfo.append(" ").append(billingCity).append(", ").append(billingState).append(" ").append(billingZip);
+
+
+        System.out.println("----------------------------------------");
+
+        shippingInfo.append("\n Shipping Address:\n");
+        shippingInfo.append(shippingStreet).append("\n");
+        shippingInfo.append(" ").append(shippingCity).append(", ").append(shippingState).append(" ").append(shippingZip);
+
+        System.out.println(addressInfo);
+        System.out.println(shippingInfo);
+
+    }
+
+}
